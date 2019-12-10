8de76782f88fd6b2f78de783d94f7989eaaa8680
Alvar Lumberg <alvar@transferwise.com>
2019-12-11 00:55:47 +0200

    day10 - first task

--- src/ee/shuhari/day10/AsteroidBase.java
+++ src/ee/shuhari/day10/AsteroidBase.java
@@ -1,10 +1,71 @@
 package ee.shuhari.day10;
 
 
+import java.util.*;
+
 public class AsteroidBase {
+  Point[][] map = new Point[0][0];
+  List<Point> asteroids = new ArrayList<>();
+  int maxVisibleAsteroids;
+  private Point bestCandidate;
+
+  static final double THRESHOLD = .0001;
+
+  public AsteroidBase(String mapString) {
+    parse(mapString);
+  }
+
+  private void parse(String mapString) {
+    String[] lines = mapString.split("\n");
+    this.map = new Point[lines[0].length()][lines.length];
+    for (int y = 0; y < lines.length; y++) {
+      String line = lines[y];
+      int index = -1;
+      int x;
+      while((x = line.indexOf("#", index+1)) > -1) {
+        index = x;
+        asteroids.add(new Point(x,y));
+      }
+    }
+  }
+
   public static void main(String[] args) {
 
 
+  }
+
+  public Point findBaseLocationWithBestVisibility() {
+    maxVisibleAsteroids = 0;
+    bestCandidate = null;
+    Map<Point,Integer> candidates = new HashMap<>();
+    for (Point candidate : asteroids) {
+      int visibleAsteroidsCount = 0;
+      for (Point another : asteroids) {
+        if(candidate != another) {
+
+          if(!isBlockedAny(candidate, another, asteroids))
+            visibleAsteroidsCount++;
+        }
+      }
+      if(visibleAsteroidsCount > maxVisibleAsteroids) {
+        maxVisibleAsteroids = visibleAsteroidsCount;
+        bestCandidate = candidate;
+      }
+    }
+    return bestCandidate;
+  }
+
+  static boolean isBlockedAny(Point candidate, Point another, List<Point> potentialBlockers) {
+    boolean blocked = false;
+    Line lineOfSight = new Line(candidate, another);
+    for (Point potentialBlocker : potentialBlockers) {
+      if(!(potentialBlocker == candidate || potentialBlocker == another)) {
+        if(lineOfSight.crosses(potentialBlocker)) {
+          blocked = true;
+        }
+      }
+    }
+    return blocked;
   }
 
   static class Point {
@@ -13,6 +74,28 @@ public class AsteroidBase {
     public Point(int x, int y) {
       this.x = x;
       this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Point point = (Point) o;
+      return x == point.x &&
+              y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+      return "Point{" +
+              "x=" + x +
+              ", y=" + y +
+              '}';
     }
   }
 
@@ -30,11 +113,16 @@ public class AsteroidBase {
       this.end = new Point(x2, y2);
     }
 
+    public Line(Point start, Point end) {
+      this.start = start;
+      this.end = end;
+    }
+
     boolean crosses(Point point) {
       double pointToStart = calcDistance(point, start);
       double pointToEnd = calcDistance(point, end);
       double startToEnd = calcDistance(start, end);
-      return pointToStart + pointToEnd == startToEnd;
+      return Math.abs(startToEnd - pointToStart - pointToEnd) < THRESHOLD;
     }
 
     double calcDistance(Point first, Point second) {
--- test/ee/shuhari/day10/AsteroidBaseTest.java
+++ test/ee/shuhari/day10/AsteroidBaseTest.java
@@ -2,21 +2,149 @@ package ee.shuhari.day10;
 
 import junit.framework.TestCase;
 
+import java.util.Arrays;
+
 public class AsteroidBaseTest extends TestCase {
 
+  static String sampleWith10TotalAsteroidsBestAt3and4 = ".#..#\n" +
+          ".....\n" +
+          "#####\n" +
+          "....#\n" +
+          "...##";
+  static String sampleWith5DetectedAsteroidsBestAt1and1 = "###\n" +
+          ".#.\n" +
+          "#.#";
+  static String sampleWith33DetectedAsteroidsBestAt5and8 = "......#.#.\n" +
+          "#..#.#....\n" +
+          "..#######.\n" +
+          ".#.#.###..\n" +
+          ".#..#.....\n" +
+          "..#....#.#\n" +
+          "#..#....#.\n" +
+          ".##.#..###\n" +
+          "##...#..#.\n" +
+          ".#....####";
+  static String sampleWith35AsteroidsBestAt1and2 = "#.#...#.#.\n" +
+          ".###....#.\n" +
+          ".#....#...\n" +
+          "##.#.#.#.#\n" +
+          "....#.#.#.\n" +
+          ".##..###.#\n" +
+          "..#...##..\n" +
+          "..##....##\n" +
+          "......#...\n" +
+          ".####.###.";
+  static String sampleWith210DetectedAsteroidsBestAt11and13 = ".#..##.###...#######\n" +
+          "##.############..##.\n" +
+          ".#.######.########.#\n" +
+          ".###.#######.####.#.\n" +
+          "#####.##.#.##.###.##\n" +
+          "..#####..#.#########\n" +
+          "####################\n" +
+          "#.####....###.#.#.##\n" +
+          "##.#################\n" +
+          "#####.##.###..####..\n" +
+          "..######..##.#######\n" +
+          "####.##.####...##..#\n" +
+          ".#####..#.######.###\n" +
+          "##...#.##########...\n" +
+          "#.##########.#######\n" +
+          ".####.#.###.###.#.##\n" +
+          "....##.##.###..#####\n" +
+          ".#.#.###########.###\n" +
+          "#.#.#.#####.####.###\n" +
+          "###.##.####.##.#..##";
+  String input = ".###.###.###.#####.#\n" +
+          "#####.##.###..###..#\n" +
+          ".#...####.###.######\n" +
+          "######.###.####.####\n" +
+          "#####..###..########\n" +
+          "#.##.###########.#.#\n" +
+          "##.###.######..#.#.#\n" +
+          ".#.##.###.#.####.###\n" +
+          "##..#.#.##.#########\n" +
+          "###.#######.###..##.\n" +
+          "###.###.##.##..####.\n" +
+          ".##.####.##########.\n" +
+          "#######.##.###.#####\n" +
+          "#####.##..####.#####\n" +
+          "##.#.#####.##.#.#..#\n" +
+          "###########.#######.\n" +
+          "#.##..#####.#####..#\n" +
+          "#####..#####.###.###\n" +
+          "####.#.############.\n" +
+          "####.#.#.##########.";
   private AsteroidBase.Line line = new AsteroidBase.Line(0, 0, 2, 2);
 
   public void testPointOnLine() {
-    AsteroidBase.Point pointOnLine = new AsteroidBase.Point(1,1);
-    AsteroidBase.Point pointNotOnLine = new AsteroidBase.Point(1,2);
+    AsteroidBase.Point pointOnLine = new AsteroidBase.Point(1, 1);
+    AsteroidBase.Point pointNotOnLine = new AsteroidBase.Point(1, 2);
 
     assertTrue(line.crosses(pointOnLine));
     assertFalse(line.crosses(pointNotOnLine));
-
-
   }
 
   public void testDistance() {
     assertEquals(2 * Math.sqrt(2), line.calcDistance(line.start, line.end));
+  }
+
+  public void testAsteroidCount() {
+    AsteroidBase base = new AsteroidBase(sampleWith10TotalAsteroidsBestAt3and4);
+    assertEquals(10, base.asteroids.size());
+  }
+
+  public void testIsBlockedByAny() {
+    AsteroidBase.Point x0y0 = new AsteroidBase.Point(0, 0);
+    AsteroidBase.Point x10y10 = new AsteroidBase.Point(10, 10);
+    AsteroidBase.Point x4y10 = new AsteroidBase.Point(4, 10);
+    AsteroidBase.Point x2y5 = new AsteroidBase.Point(2, 5);
+
+    assertFalse(AsteroidBase.isBlockedAny(x0y0, x10y10, Arrays.asList(x0y0, x10y10, x4y10, x2y5)));
+
+  }
+
+  public void testIfCrosses() {
+    AsteroidBase.Point x0y0 = new AsteroidBase.Point(0, 0);
+    AsteroidBase.Point x10y10 = new AsteroidBase.Point(10, 10);
+    AsteroidBase.Point x4y10 = new AsteroidBase.Point(4, 10);
+    AsteroidBase.Point x2y5 = new AsteroidBase.Point(2, 5);
+
+    AsteroidBase.Point x1y0 = new AsteroidBase.Point(1, 0);
+    AsteroidBase.Point x3y2 = new AsteroidBase.Point(3, 2);
+    AsteroidBase.Point x4y3 = new AsteroidBase.Point(4, 3);
+
+    assertTrue(new AsteroidBase.Line(x0y0, x4y10).crosses(x2y5));
+    assertFalse(new AsteroidBase.Line(x0y0, x10y10).crosses(x2y5));
+    assertFalse(new AsteroidBase.Line(x0y0, x10y10).crosses(x4y10));
+
+    assertTrue(new AsteroidBase.Line(x1y0, x4y3).crosses(x3y2));
+  }
+
+  public void testFindAsteroidWithMostVisibleOthers() {
+    AsteroidBase base = new AsteroidBase(sampleWith5DetectedAsteroidsBestAt1and1);
+    assertEquals(new AsteroidBase.Point(1, 0), base.findBaseLocationWithBestVisibility());
+    assertEquals(5, base.maxVisibleAsteroids);
+
+    base = new AsteroidBase(sampleWith10TotalAsteroidsBestAt3and4);
+    assertEquals(new AsteroidBase.Point(3, 4), base.findBaseLocationWithBestVisibility());
+    assertEquals(8, base.maxVisibleAsteroids);
+
+    base = new AsteroidBase(sampleWith33DetectedAsteroidsBestAt5and8);
+    assertEquals(new AsteroidBase.Point(5, 8), base.findBaseLocationWithBestVisibility());
+    assertEquals(33, base.maxVisibleAsteroids);
+
+    base = new AsteroidBase(sampleWith35AsteroidsBestAt1and2);
+    assertEquals(new AsteroidBase.Point(1, 2), base.findBaseLocationWithBestVisibility());
+    assertEquals(35, base.maxVisibleAsteroids);
+
+    base = new AsteroidBase(sampleWith210DetectedAsteroidsBestAt11and13);
+    assertEquals(new AsteroidBase.Point(11, 13), base.findBaseLocationWithBestVisibility());
+    assertEquals(210, base.maxVisibleAsteroids);
+  }
+
+  public void testInput() {
+    AsteroidBase base = new AsteroidBase(input);
+    base.findBaseLocationWithBestVisibility();
+    System.out.println(base.maxVisibleAsteroids);
   }
 }
